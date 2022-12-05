69bd37920e2818a7602ac26df030785ef8c7363f
John-Michael Krsak <jmkrsak@gmail.com>
2022-12-05 13:26:09 -0500

    added condtructor/getters/setters

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -2,10 +2,40 @@ import java.util.ArrayList;
 
 public class Student {
 
-    long id;
+    private long id;
 
-    String name;
+    private String name;
 
-    ArrayList<Integer> grades;
+    private ArrayList<Integer> grades;
+
+    public Student(String name, long id, ArrayList<Integer> grades) {
+        this.name = name;
+        this.id = id;
+        this.grades = grades;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return grades;
+    }
+
+    public void setGrades(ArrayList<Integer> grades) {
+        this.grades = grades;
+    }
 
 }
