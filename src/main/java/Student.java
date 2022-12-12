fb785abd94e504ec9fa6823d98419eeeb601a668
John-Michael Krsak <jmkrsak@gmail.com>
2022-12-12 09:54:49 -0500

    update

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -8,4 +8,46 @@ public class Student {
 
     ArrayList<Integer> grades;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return grades;
+    }
+
+    public void setGrades(ArrayList<Integer> grades) {
+        this.grades = grades;
+    }
+
+    public Student(long id, String name, ArrayList grade) {
+        this.id = id;
+        this.name = name;
+        this.grades = grade;
+    }
+
+    public void addGrade(int grade) {
+        grades.add(grade);
+    }
+
+    public double getAverage() {
+        int sum = 0;
+        for (int i = 0; i < grades.size(); i++) {
+            sum += grades.get(i);
+        }
+        return sum / grades.size();
+    }
+
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,7 +1,31 @@
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class StudentTest {
 
+    Student empty;
+    Student studentWithOne;
+    Student studentWithMany;
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void testSize() {
+
+    }
+
+    @Test
+    public void testIsEmpty() {
+
+    }
+
+    @Test
+    public void testAdd() {
+
+    }
 
 }
